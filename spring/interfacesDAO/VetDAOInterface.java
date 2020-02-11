5dbf0a662bc32d9a2f4ac7e32376d839b8c12394
theBossLorenzo <lorenzohandula@gmail.com>
2020-02-11 20:23:00 -0300

    soy un upo

---
+++ spring/interfacesDAO/VetDAOInterface.java
@@ -0,0 +1,8 @@
+package ttps.spring.interfacesDAO;
+
+import ttps.spring.model.Vet;
+
+public interface VetDAOInterface extends GenericDAOInterface<Vet> {
+	 public Vet recuperarVet(Long id);
+	 public Vet update(Vet vet);
+	}
